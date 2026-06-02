ec2f8c2de3976eb9e8127a1377f5c4cee4e504fa
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-06-02 16:43:53 +0530

    Export data from the Dynamodb and import to the S3

--- Scripts/Utils/Upload_dataset.py
+++ Scripts/Utils/Upload_dataset.py
@@ -5,7 +5,13 @@ load_dotenv()
 
 s3=boto3.client('s3')
 file_path = "../../data/data1.csv"
+file_path1="../../data/student_data.csv"
+file_path2="../../data/student_data.json"
 bucketName=os.getenv('bucket_name')
 s3_key=os.getenv('s3_keys')
-s3.upload_file(file_path, bucketName, s3_key)
+s3_keys=os.getenv('s3_k')
+s3_k=os.getenv('s3_ke')
+# s3.upload_file(file_path, bucketName, s3_key)
+s3.upload_file(file_path1, bucketName, s3_keys)
+s3.upload_file(file_path2, bucketName, s3_keys)
 print("File uploaded successfully")
